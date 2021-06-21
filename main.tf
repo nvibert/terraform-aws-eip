25dadfdf352d1d0f68e55eebe329a82ceae386ed
Nico Vibert <nvibert@vmware.com>
2021-06-21 12:04:43 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+data "terraform_remote_state" "vpc" {
+  backend = "remote"
+  config = {
+    organization = "nvibert"
+    workspaces = {
+      name = "terraform-aws-sentinel"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_eip" "eip" {
+  instance = data.terraform_remote_state.vpc.outputs.instance_ami_id
+  vpc      = true
+}
+
+output "eip" {
+  value = aws_eip.eip.public_ip
+}
